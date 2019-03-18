0de22ffd1610197d39ffc616fb1c984c9092e8c9
migs432 <migs910@hotmail.com>
2019-03-18 22:55:01 +0000

    adding function old school way

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,41 +1,44 @@
 import React, { Component } from 'react';
 
 
+
+
+
 class TodoItem extends Component {
-    
-    
-   getStyle = () => {
-    
-                    if(this.props.todo.completed){
-                                                      return {textDecoration:'line-through'}  
-                                                     }
-                                                     
-                    else{return{textDecoration:'none'}}  
-                    
-                    }
-    
-    
-                              render() {
-                
-                                        return (
-                                                
-                                                <div  style={this.getStyle()}>
-                                                
-                                                    <p>{this.props.todo.title}</p>
-                                                    
-                                                </div>
-                                            
-                                                )
-                                      }
+
+                                      render() {
+                        
+                                                    return (
+                                                            
+                                                            <div  style={itemStyle}>
+                                                            
+                                                                <p style={getStyle(this.props) } >  {this.props.todo.title}   </p>
+                                                                
+                                                            </div>
+                                                        
+                                                            )
+                                              }
                                       
                                       
                             
-                            }
+                               }
                             
 
 var itemStyle = {backgroundColor:'yellow'} 
 
 
+
+function getStyle(props)  {
+    
+                            if(props.todo.completed){
+                                
+                                                      return {textDecoration:'line-through'}  
+                                                      
+                                                     }
+                                                             
+                            else{  return{textDecoration:'none'}  }  
+                            
+                            }  
 
 
 export default TodoItem;
