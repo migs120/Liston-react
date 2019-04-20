f511abe71e2c61f783e50144af976a8e96736816
migs432 <migs910@hotmail.com>
2019-04-20 23:05:06 +0000

    changed to previous commit to keep app similar to miggymigs for developing on miggymigs

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from "prop-types"
+import App from '../App'
+
+class HelloWorld extends React.Component {
+                                            render () {
+                                                        
+                                                       return(
+                                                           
+                                                              <App/>
+                                                           
+                                                              ) 
+                                                               
+                                                      }
+                                          }
+                                          
+                                          
+
+
+                      
+export default HelloWorld
