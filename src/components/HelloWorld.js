ce94ce10e1021fd020711e2dae63561321d513fb
migs432 <migs910@hotmail.com>
2019-04-20 23:07:26 +0000

    added helloword to make similar to miggymigs

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -15,6 +15,7 @@ class HelloWorld extends React.Component {
                                           }
                                           
                                           
+                                          
 
 
                       
